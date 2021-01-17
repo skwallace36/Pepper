282cb77ec7e08db7a94833a616e7d66d5aff7713
Stuart Wallace <swallace@fitbit.com>
2021-01-16 19:35:36 -0500

    Initial Commit

---
+++ Pepper/PepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PepperApp.swift
+//  Pepper
+//
+//  Created by Stuart Wallace on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct PepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
